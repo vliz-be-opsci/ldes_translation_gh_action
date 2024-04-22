5836dec3098ede65c80d709cda5336257a6d8d46
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-22 11:35:48 +0200

    Update LDES download script to install @treecg/actor-init-ldes-client

--- .github/ldes_translation_workflow/ldes_download.sh
+++ .github/ldes_translation_workflow/ldes_download.sh
@@ -22,6 +22,8 @@ python -c "import json; [print(json.dumps(source)) for source in json.loads('$co
         mkdir "../../$source_name"
     fi
 
+    # check if npm i @treecg/actor-init-ldes-client is installed
+    npm i @treecg/actor-init-ldes-client
     # CLI command to harvest data from a LDES
     actor-init-ldes-client --pollingInterval 5000 --mimeType text/turtle --emitMemberOnce true --disablePolling true --requestsPerMinute 40 "$source_url" > "../../$source_name/output_ldes_stream.ttl"
 
